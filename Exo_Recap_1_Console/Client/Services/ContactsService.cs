4ea66a3fa760ab0cf7c43f92fc04ebf205866876
hexavoyager <hexavoyager@gmail.com>
2021-06-10 11:30:16 +0200

    Passage a l'etape 10

---
+++ Exo_Recap_1_Console/Client/Services/ContactsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Connections.Database;
+using GContactService = Exo_Recap_1_Console.ContactsService;
+using Exo_Recap_1_Console.Client.Mappers;
+using C = Exo_Recap_1_Console.Client.Data;
+
+namespace Exo_Recap_1_Console.Client.Services
+{
+    class ContactsService
+    {
+        private readonly GContactService _contactService;
+
+        public ContactsService(GContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        public IEnumerable<Contact> Get()
+        {
+            return _contactService.Get();
+        }
+        public IEnumerable<Contact> GetByCategory(int id)
+        {
+            return _contactService.GetByCategory(id);
+        }
+        public Contact Get(int id)
+        {
+            return _contactService.Get(id);
+        }
+        public bool Insert(Contact contact)
+        {
+            return _contactService.Insert(contact);
+        }
+        public bool Update(int id, Contact contact)
+        {
+            return _contactService.Update(id, contact);
+        }
+        public bool Delete(int id)
+        {
+            return _contactService.Delete(id);
+        }
+
+
+    }
+}
